6cc66cf2c425115c30e758f5ee2feaac683b1b32
Dmitry Smirnov <dimasm2001@mail.ru>
2022-02-26 23:06:12 +0300

    lesson_01 ready

--- todonotes/todonotes/settings.py
+++ todonotes/todonotes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'userapp',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userapp.User'
